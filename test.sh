c5e2bdf3ec6ba1e296c68f34e550e7f9d1441d10
mouradmourafiq <mouradmourafiq@gmail.com>
2017-11-23 19:44:24 +0100

    Add tests

---
+++ test.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 1 ]
+  then
+    echo "You should provide 1 args"
+     exit 1
+fi
+
+PY=python
+if [ $1 -eq 3 ]
+then
+    PY=python3
+fi
+echo "Python interpreter $PY"
+
+
+echo "Testing base images:"
+
+echo "Pulling docker image base:$1"
+if [ $1 -eq 3 ]
+  then
+    docker pull polyaxon/polyaxon-base
+    docker pull polyaxon/polyaxon-npm-base
+  else
+    docker pull polyaxon/polyaxon-base2
+    docker pull polyaxon/polyaxon-npm-base2
+fi
+
+
+
+echo "Running py2 base images tests"
+for image in polyaxon/polyaxon-base2 polyaxon/polyaxon-npm-base2
+do
+    echo "Test for python2"
+    docker run $image python --version
+done
+
+
+echo "Running py3 base images tests"
+for image in polyaxon/polyaxon-base polyaxon/polyaxon-npm-base
+do
+    echo "Test for python2"
+    docker run $image python3 --version
+done
